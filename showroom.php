43a012d69519f5b2e37b243b5c10cc79c4bd7eb9
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2019-11-12 14:48:02 +0600

    showroom table added

--- showroom.php
+++ showroom.php
@@ -1,5 +1,9 @@
 <?php include 'header.php'; ?>
         <!-- =-=-=-=-=-=-= Main Area =-=-=-=-=-=-= -->
+		<!-------------------------------------Datatables----->
+        <link rel="stylesheet" href="frontend/css/bootstrap.min.css">
+        <link rel="stylesheet" href="frontend/css/dataTables.bootstrap.min.css">
+		<!-------------------------------------Datatables----->
         <div class="main-content-area">
             <!-- =-=-=-=-=-=-= Latest Questions  =-=-=-=-=-=-= -->
             <section class="section-padding-80 white" id="questions">
@@ -10,6 +14,38 @@
                             <div class="jumbotron text-center">
                                 <h1 class="service_title">Showroom Information</h1>
                             </div>
+							<table id="example" class="table table-striped table-bordered" style="width:100%">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Position</th>
+										<th>Office</th>
+										<th>Age</th>
+										<th>Start date</th>
+										<th>Salary</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+									</tr>
+								</tbody>
+								<tfoot>
+									<tr>
+										<th>Name</th>
+										<th>Position</th>
+										<th>Office</th>
+										<th>Age</th>
+										<th>Start date</th>
+										<th>Salary</th>
+									</tr>
+								</tfoot>
+							</table>
                         </div>                        
                     </div>
                     <!-- Row End -->
