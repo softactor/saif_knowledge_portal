27d048acabbe23b03dc6050c07b269ca102bc3f0
softactor <tanveerqureshee1@gmail.com>
2020-01-21 10:01:37 +0600

    division added in product and showroom close

--- frontend/modal/showroom_details_modal.php
+++ frontend/modal/showroom_details_modal.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div id="showroomDetailsModal" class="modal fade" role="dialog">
-    <div class="modal-dialog">
+    <div class="modal-dialog" style="width:95%">
         <!-- Modal content-->
         <div class="modal-content">
             <span id="showroomDetailsBody"></span>
